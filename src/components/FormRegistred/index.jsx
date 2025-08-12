917150200ead48bf9e8ef3f5c2c5af07c1d75fed
Aliaksandr nest <aliaksandrnest@Air-Aliaksandr.bbrouter>
2025-08-12 20:30:36 +0300

    dsadas

--- src/components/FormRegistred/index.jsx
+++ src/components/FormRegistred/index.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import { Button, Flex } from "antd";
 
 const FormRegistred = () => {
-    
-  const userInfo = {
-    id: 1751,
-    username: "220marry",
-    email: "220example@example.com",
-    gender: "female",
-    age: 25,
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    gender: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      alert("Заполните поля");
+    }
+
+    if (formData.username.length < 2 || formData.username.length > 20) {
+      alert("Имя может быть от 2-ух символов до 25");
+    }
   };
 
   const userRegistred = async () => {
@@ -21,9 +39,12 @@ const FormRegistred = () => {
             accept: "application/json",
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(userInfo),
+          body: JSON.stringify(formData),
         }
       );
+      if (!response.ok) {
+        throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
+      }
       const data = await response.json();
       console.log(data);
     } catch (error) {
@@ -35,15 +56,33 @@ const FormRegistred = () => {
     <>
       <div className="form">
         <h1>Регистрация пользователя:</h1>
-        <form>
+        <form onSubmit={submit}>
           <label>Имя пользователя:</label>
-          <input type="text" placeholder="Введите ваш никнейм" />
-          <label>email:</label>
-          <input type="email" placeholder="Введите ваш email" />
+          <input
+            type="text"
+            name="username"
+            placeholder="Введите ваш никнейм"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Введите ваш email"
+            value={formData.email}
+            onChange={handleChange}
+          />
           <label>Пароль:</label>
-          <input type="text" placeholder="Введите ваш пароль" />
+          <input
+            type="password"
+            name="password"
+            placeholder="Введите ваш пароль"
+            value={formData.password}
+            onChange={handleChange}
+          />
           <label>Пол:</label>
-          <select name="gender">
+          <select name="gender" value={formData.gender} onChange={handleChange}>
             <option value="">Выберите пол</option>
             <option value="male">Мужской</option>
             <option value="female">Женский</option>
@@ -51,9 +90,12 @@ const FormRegistred = () => {
           <label>Возраст:</label>
           <input
             type="number"
+            name="age"
             min={0}
             max={100}
             placeholder="Введите ваш возраст"
+            value={formData.age}
+            onChange={handleChange}
           />
         </form>
         <Flex gap="small" wrap>
